b6bf7f5f91cd815a7e68d602fe3c797a65925dc4
BryLegHome <Brylegrain@gmail.com>
2022-01-22 20:19:34 +0100

    galleryChanges+cssChanges

--- api/displayProjects.js
+++ api/displayProjects.js
@@ -1,11 +1,12 @@
-const container = document.querySelector('.images')
+const container = document.querySelector('.gallery')
 
 const displayProjects = (projects) => {
-    const { name, id, url, image } = projects;
-    return container.innerHTML = `<a href="${url} "data-id='${id}'>
+    const { name, id, url, image, infos } = projects;
+    return container.innerHTML = `<h2 class="projectTitle">${name}</h2> 
+    <a href="${url} "data-id='${id}'>
     <img class="imgProject" src="${image}" alt="image ${name}">
     </a>
-    <h2 class="projectTitle">${name}</h2> `
+    <p class="projectInfos">${infos}</p>`
 }
 
 export default displayProjects;
--- app.js
+++ app.js
@@ -15,8 +15,8 @@ window.addEventListener("DOMcontentLoaded", showProjects(0));
 const btns = document.querySelectorAll('.btnProject');
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-
-        const id = parseInt(e.currentTarget.parentElement.previousElementSibling.firstChild.dataset.id);
+        console.log(e.currentTarget.parentElement.previousElementSibling.f);
+        const id = parseInt(e.currentTarget.parentElement.previousElementSibling.firstChild.nextElementSibling.dataset.id);
         e.currentTarget.classList.contains('btnNext') ? id == 2 ? showProjects(0) : showProjects(id + 1) : "";
         e.currentTarget.classList.contains('btnPrev') ? id == 0 ? showProjects(2) : showProjects(id - 1) : "";
 
