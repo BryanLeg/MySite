abfb569ec8f6716b85b0a42b69dcb1438479faa7
BryanLeg <Brylegrain@gmail.com>
2022-03-11 19:52:23 +0100

    update languages/ frameworks/CV

--- app.js
+++ app.js
@@ -4,33 +4,37 @@ import displayProjects from "./api/displayProjects.js";
 let length;
 
 const showProjects = async (index) => {
-    const projects = await getProjects();
-    length = projects.length;
-    displayProjects(projects[index]);
-}
+  const projects = await getProjects();
+  length = projects.length;
+  displayProjects(projects[index]);
+};
 
 window.addEventListener("DOMcontentLoaded", showProjects(0));
 
-
-const btns = document.querySelectorAll('.btnProject');
+const btns = document.querySelectorAll(".btnProject");
 btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
+  btn.addEventListener("click", (e) => {
+    if (e.currentTarget.classList.contains("btnNext")) {
+      const idNext = parseInt(
+        e.currentTarget.previousElementSibling.firstChild.nextElementSibling
+          .dataset.id
+      );
 
-        if (e.currentTarget.classList.contains('btnNext')) {
-            const idNext = parseInt(e.currentTarget.previousElementSibling.firstChild.nextElementSibling.dataset.id);
-
-            if (idNext === length - 1) {
-                showProjects(0)
-            } else {
-                showProjects(idNext + 1)
-            }
-        } else if (e.currentTarget.classList.contains('btnPrev')) {
-            const idPrev = parseInt(e.currentTarget.nextElementSibling.firstChild.nextElementSibling.dataset.id);
-            if (idPrev == 0) {
-                showProjects(length - 1)
-            } else {
-                showProjects(idPrev - 1)
-            }
-        }
-    })
-})
+      if (idNext === length - 1) {
+        showProjects(0);
+      } else {
+        showProjects(idNext + 1);
+      }
+    } else if (e.currentTarget.classList.contains("btnPrev")) {
+      const idPrev = parseInt(
+        e.currentTarget.nextElementSibling.firstChild.nextElementSibling.dataset
+          .id
+      );
+      if (idPrev == 0) {
+        showProjects(length - 1);
+      } else {
+        showProjects(idPrev - 1);
+      }
+    }
+  });
+});
